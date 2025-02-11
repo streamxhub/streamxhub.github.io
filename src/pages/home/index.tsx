7f82e67e2a11fe68a58fdf1e1aa34430716935a3
kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2025-02-11 11:26:22 +0800

    style: change feature icon and color

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -20,6 +20,7 @@ import { useTranslation } from '@site/src/hooks/useTranslation';
 
 import './style.less';
 import BannerVideo from '@site/src/components/BannerVideo';
+import { featureCardStyle } from './feature.data';
 
 export default function HomePage() {
   const INSTALL_COMMAND = `curl -L https://streampark.apache.org/quickstart.sh | sh`;
@@ -40,43 +41,43 @@ export default function HomePage() {
     };
   }, []);
   return (
-    <div className="homepage-wrapper overflow-hidden">
+    <div className='homepage-wrapper overflow-hidden'>
       <div
-        data-aos="fade-down"
-        className="absolute top-[60px] right-0 h-[300px] w-[300px] overflow-hidden z-[-1]"
+        data-aos='fade-down'
+        className='absolute top-[60px] right-0 h-[300px] w-[300px] overflow-hidden z-[-1]'
       >
-        <TopWaveImage className="absolute bottom-0 right-0  w-[1400px] -rotate-6" />
+        <TopWaveImage className='absolute bottom-0 right-0  w-[1400px] -rotate-6' />
       </div>
 
-      <section className="banner-section container mt-16 flex flex-col">
+      <section className='banner-section container mt-16 flex flex-col'>
         <BannerTitle />
         <div
-          className="flex justify-center space-x-6 text-sm"
-          data-aos="fade-right"
-          data-aos-duration="6000"
-          data-aos-offset="100"
+          className='flex justify-center space-x-6 text-sm'
+          data-aos='fade-right'
+          data-aos-duration='6000'
+          data-aos-offset='100'
         >
           <GetStartButton>Get Start</GetStartButton>
           <GitHubButton>View on GitHub</GitHubButton>
         </div>
       </section>
 
-      <Browser className="!h-auto bg-shadow-primary">
+      <Browser className='!h-auto bg-shadow-primary'>
         <BannerVideo />
       </Browser>
 
-      <section data-aos="fade-up" className="flex flex-col items-start my-10">
-        <div className="text-base lg:text-xl text-neutral-600 dark:text-neutral-400 mx-auto mb-4">
+      <section data-aos='fade-up' className='flex flex-col items-start my-10'>
+        <div className='text-base lg:text-xl text-neutral-600 dark:text-neutral-400 mx-auto mb-4'>
           Start using Apache StreamPark™ in minutes
         </div>
-        <div className="mx-auto px-2">
+        <div className='mx-auto px-2'>
           <ShellCommand
             command={INSTALL_COMMAND}
-            className="max-w-[80vw] md:max-w-full"
+            className='max-w-[80vw] md:max-w-full'
           />
-          <p className="text-xs md:text-sm text-neutral-400 dark:text-neutral-600 mx-auto mt-2 pl-2">
+          <p className='text-xs md:text-sm text-neutral-400 dark:text-neutral-600 mx-auto mt-2 pl-2'>
             <span>Or install other release version from the&nbsp;</span>
-            <a href="/download" className="text-primary-400">
+            <a href='/download' className='text-primary-400'>
               download page
             </a>
             <span>.</span>
@@ -84,43 +85,45 @@ export default function HomePage() {
         </div>
       </section>
 
-      <div className="flex justify-end" data-aos="slide-left">
-        <BottomWaveImage className="h-[120px] md:h-[250px] w-full shrink-0 mt-[36px] md:mt-[75px]" />
-        <BottomWaveImage className="h-[120px] md:h-[250px] w-full shrink-0" />
+      <div className='flex justify-end' data-aos='slide-left'>
+        <BottomWaveImage className='h-[120px] md:h-[250px] w-full shrink-0 mt-[36px] md:mt-[75px]' />
+        <BottomWaveImage className='h-[120px] md:h-[250px] w-full shrink-0' />
       </div>
 
-      <section className="lg:pb-20">
+      <section className='lg:pb-20'>
         <SectionTitle title={t.users.title} description={''} />
         <BrowserOnly>{() => <TrustedUsers />}</BrowserOnly>
       </section>
 
-      <section className="p-0">
+      <section className='p-0'>
         <BrowserOnly>{() => <AchievementBanner />}</BrowserOnly>
       </section>
 
-      <section className="lg:py-20 bg-[#f7f9fe] dark:bg-[#1a1b1c]">
-        <div className="w-full overflow-hidden">
+      <section className='lg:py-20 bg-[#f7f9fe] dark:bg-[#1a1b1c]'>
+        <div className='w-full overflow-hidden'>
           <SectionTitle
             title={t.common.coreFeatures}
             description={t.common.coreFeaturesDesc}
           />
-          <div className="flex-center flex-wrap">
+          <div className='flex-center flex-wrap'>
             {t.feature.map((feature, i) => (
               <FeatureCard
                 key={i}
+                id={feature.id}
                 title={feature.title}
-                icon={feature.icon}
+                icon={featureCardStyle[feature.id]?.icon}
+                style={featureCardStyle[feature.id]?.style}
                 content={feature.details}
                 animationDelay={(i % 3) * 100}
-                className="hover:-translate-y-[10px] transition-transform ease-in-out duration-300"
+                className='hover:-translate-y-[10px] transition-transform ease-in-out duration-300'
               />
             ))}
           </div>
         </div>
       </section>
 
-      <section className="lg:py-20">
-        <div className="w-full overflow-hidden">
+      <section className='lg:py-20'>
+        <div className='w-full overflow-hidden'>
           <SectionTitle
             title={t.common.development}
             description={t.common.developmentDesc}
